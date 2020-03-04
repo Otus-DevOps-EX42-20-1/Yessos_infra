43572d5f42e93d126927686f6ae926ec78afb559
Yessos <iakrevetko@gmail.com>
2020-03-04 14:35:28 +0300

    Reddit app is being provisioned through ansible now as well as packer

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -2,3 +2,12 @@ output "app_external_ip" {
   value       = module.app.app_external_ip
   description = "External IP for Reddit App"
 }
+
+output "db_internal_ip" {
+  value = module.db.db_internal_ip
+}
+
+output "db_external_ip" {
+  value       = module.db.db_external_ip
+  description = "External IP for DB Instance"
+}
